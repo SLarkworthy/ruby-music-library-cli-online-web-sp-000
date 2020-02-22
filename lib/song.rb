0e26d45e1fdd9d8ee6c0cd9708210b326bed78d3
Sarah Larkworthy <sarah.larkworthy@gmail.com>
2020-02-22 19:12:30 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,54 @@
+class Song
+  attr_accessor :name, :artist, :genre
+  
+  @@all = []
+  
+  def initialize(name, artist = nil, genre = nil)
+    @name = name
+    self.artist = artist if artist != nil
+    self.genre = genre if genre != nil
+  end
+  
+  def genre=(genre)
+    @genre = genre
+    genre.songs << self if !genre.songs.include?(self)
+  end
+  
+  def artist=(artist)
+    #song.artist = 
+    @artist = artist 
+    artist.add_song(self)
+  end
+  
+  def save
+    @@all << self 
+  end
+  
+  def self.all 
+    @@all 
+  end
+  
+  def self.destroy_all
+    @@all.clear
+  end
+  
+  def self.create(name, artist = nil, genre = nil)
+   song = self.new(name, artist = nil, genre = nil)
+   song.save
+   song
+  end
+  
+  def self.find_by_name(name)
+    @@all.find{|song| song.name == name}
+  end
+  
+  def self.find_or_create_by_name(name)
+    self.find_by_name(name) ||= self.create(name)
+    #   self.create(name)
+    # else
+    #   self.find_by_name(name)
+    # end
+  end
+      
+  
+end
