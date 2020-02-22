3bc276f1d284dc642981f4057656b64454756606
Sarah Larkworthy <sarah.larkworthy@gmail.com>
2020-02-22 20:17:41 +0000

    Automatically backed up by Learn

--- lib/MusicLibraryController.rb
+++ lib/MusicLibraryController.rb
@@ -29,7 +29,8 @@ class MusicLibraryController
   
   def list_songs
     #we will need all the songs plus their indexes to number the list.
-    Song.all.each_with_index
+    #Song.all will give us alll instances of the song class
+    #Song.all.collect{|s| s.name}.sort 
   end
   
 end
