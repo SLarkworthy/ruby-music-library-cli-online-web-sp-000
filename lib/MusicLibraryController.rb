196ce1cb8382427c91deea39e7e827576b6fa10f
Sarah Larkworthy <sarah.larkworthy@gmail.com>
2020-02-22 20:09:29 +0000

    Automatically backed up by Learn

--- lib/MusicLibraryController.rb
+++ lib/MusicLibraryController.rb
@@ -11,7 +11,8 @@ class MusicLibraryController
   def call 
     input = gets.strip
     
-    if input != "exit"
+    if (input != "exit")
+      
       puts "Welcome to your music library!"
       puts "To list all of your songs, enter 'list songs'."
       puts "To list all of the artists in your library, enter 'list artists'."
@@ -21,6 +22,7 @@ class MusicLibraryController
       puts "To play a song, enter 'play song'."
       puts "To quit, type 'exit'."
       puts "What would you like to do?"
+      
     end
     
   end
