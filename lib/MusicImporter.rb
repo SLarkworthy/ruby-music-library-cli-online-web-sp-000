b4df516ccfb5363f85a2e2cd627da1db6e8aefc6
Sarah Larkworthy <sarah.larkworthy@gmail.com>
2020-02-22 19:23:58 +0000

    Automatically backed up by Learn

--- lib/MusicImporter.rb
+++ lib/MusicImporter.rb
@@ -9,7 +9,7 @@ class MusicImporter
     puts Dir.entries(@path)
     
     # Dir.entries(@path).select! {|entry| entry.end_with?(".mp3")}
-    
+    # @files ||= Dir.glob("#{path}/*.mp3").collect{ |f| f.gsub("#{path}/", "") }
   end
   
   def self.import 
